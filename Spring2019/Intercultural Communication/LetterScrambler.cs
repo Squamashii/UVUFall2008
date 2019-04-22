4dd785bad89f2f7193765ac13fa059da4e695204
Squamashii <spencerbarber.biz@gmail.com>
2019-04-22 13:20:10 -0600

    Word Scrambler Scripts

---
+++ Spring2019/Intercultural Communication/LetterScrambler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterScrambler : MonoBehaviour {
+
+	List<GameObject> tempWord;
+	
+	public void Randomizer(List<GameObject> word){
+		int letter1 = Random.Range(0, word.Capacity-1);
+		int letter2 = Random.Range(0, word.Capacity-1);
+
+		GameObject temp = word[letter1];
+		word[letter1] = word[letter2];
+		word[letter2] = temp;
+	}
+}
---
+++ Spring2019/Intercultural Communication/WordObject.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordObject : MonoBehaviour {
+
+	public List<GameObject> newWord;
+	private List<GameObject> defaultWord;
+	
+	public float spacing = 2;
+
+	public LetterScrambler scrambler;
+
+	void Start(){
+		SaveWord();
+		BuildWord(false);
+	}
+	
+	void SaveWord(){
+		defaultWord = new List<GameObject> (newWord);
+	}
+
+	public void Reset(){
+		newWord = new List<GameObject> (defaultWord);
+		BuildWord(false);
+	}
+
+	public void Scramble()
+	{
+		BuildWord(true);
+	}
+
+	private void BuildWord(bool scramble){
+
+		if(scramble)
+		{
+			scrambler.Randomizer(newWord);
+		}
+
+		foreach(GameObject letter in newWord)
+		{
+			letter.transform.localPosition = Vector3.zero;
+		}
+
+		float tempSpacing = spacing;
+		foreach (GameObject letter in newWord)
+		{
+			letter.transform.Translate(tempSpacing,0,0);
+			tempSpacing += spacing;
+		}	
+	}
+}
